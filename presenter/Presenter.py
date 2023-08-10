1c205873c47367de933d72c1b3e5acc08a6d5e10
alex123 <alex123@dot.com>
2023-08-10 19:05:59 +0500

    MVP реализован

--- presenter/Presenter.py
+++ presenter/Presenter.py
@@ -1,5 +1,5 @@
 from model import Service
-from view import View
+from view import View, ConsoleUI
 
 
 class Presenter:
@@ -8,5 +8,18 @@ class Presenter:
         self.service = Service.Service()
         self.view = View.View()
 
+    def print_notes(self, numCommand):
+        self.service.print_notes(numCommand)
+
+    def add_note(self, title, body):
+        self.service.add_note(title, body)
+
+    def action_byId(self):
+        self.service.action_byId()
+
+    def dialog(self, text_out)-> str:
+        return ConsoleUI.ConsoleUI().dialog(text_out)
     
+    def printAnswer(self, text):
+        ConsoleUI.ConsoleUI().printAnswer(text)
 
